5c5a5d5f523befafa3d73282dbb3f9006a6f9fe0
DiogoSouza <diogohg24@gmail.com>
2025-08-27 20:16:58 -0300

    Sistema de saber quel o numero e maior entre 3 inteiros

---
+++ Maior.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int a, b, c;
+int maior;
+
+cout<<"Digite o valor de A: ";
+cin>>a;
+cout<<"Digite o valor de B: ";
+cin>>b;
+cout<<"Digite o valor de C: ";
+cin>>c;
+
+if((a>b) && (a>c)){
+    maior=a;
+}else {
+    if ((b>a) && (b>c)){
+        maior=b;
+    }else
+        maior=c;
+}
+cout<<"O maior Sera: "<<maior;
+
+return 0;
+
+
+
+
+
+
+}
