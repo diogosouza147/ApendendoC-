117fd6f66c8631ad9fb8f5119ed0276e1cc947d5
diogosouza147 <diogohg24@gmail.com>
2025-09-17 16:52:22 -0300

    estatistica

--- estatisticas.cpp
+++ estatisticas.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main() {
-    int numeros[] = {64, 6, 2, 12, 77, 11, 52};
-    int tamanho = 7;
-    int soma = 0;
+    int numeros[] = {3, 7, 2, 8, 5, 1};
+    int tamanho = 6;
+    double soma;
+    int vetorPar[6];
+    int vetorImpar[6];
+    double media;
+    double mediana;
 
     for(int i = 0; i < tamanho - 1; i++) {
         int indiceMenor = i;
@@ -25,5 +29,53 @@ int main() {
 	 for(int i = 0; i < tamanho; i++) {
         cout << numeros[i] << " ";
     }  
+
+    cout<<endl<<endl;
+
+    for (int i = 0; i <6 ; i++)
+    {
+        if (numeros[i] % 2 == 0)
+        {
+            vetorPar[i] = numeros[i];
+            cout<<"Vetor Par: "<<vetorPar[i]<<endl;
+        }
+        else
+        {
+            vetorImpar[i] = numeros[i];
+            cout<<"Vetor Impar: "<<vetorImpar[i]<<endl;
+        }
+        
+    }
+
+    cout<<endl;
+    cout<<"A media dos valores do vetor e: ";
+    for (int i = 0; i <tamanho; i++)
+    {
+        soma = soma += numeros[i];
+    }
+
+    media = soma / tamanho;
+    cout<<media<<endl;
+    
+    cout<<endl<<endl;
+    cout<<"A mediana é: "<<endl;
+    if (tamanho % 2 != 0)
+    {
+        mediana = numeros[tamanho / 2];
+        cout<<mediana<<endl;
+    }
+    else
+    {
+        mediana = (numeros[tamanho/2 -1 ] + numeros[tamanho/2]) /2.0;
+        cout<<mediana<<endl;
+    }
+    
+
+
     return 0;
+
+
+
+
+
 }
